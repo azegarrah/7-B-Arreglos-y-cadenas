93dfc2649aa5cb92878956d474187aa340004140
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-18 23:58:41 -0500

    Puse los mensajes del codigo ya hecho que funcionaba

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main(){
     int N, A[1000];
+    cout << "Ingrese la cantidad de datos: ";
     cin >> N;
 
+    cout << "Ingrese los datos: ";
     for(int i = 0; i < N; i++){
         cin >> A[i];
     }
+    cout << "Los datos de forma reversa son: ";
     for(int i = N - 1; i >= 0; i--){
         cout << A[i] << " ";
     }
-    
+
     return 0;
 }
