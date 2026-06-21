0a88cb8f64df18ae7110d5f0975d845206f1d9ea
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-20 23:17:27 -0500

    Uso del for para leer la cantidad de votos en el arreglo frecuencia

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -6,12 +6,10 @@ int main(){
     int P, n, F, frecuencia[101] = {0};
     cin >> P >> n;
 
-
-
-
-
-
-
+    for(int i = 0; i < n; i++){
+        cin >> F;
+        frecuencia[F]++;
+    }
 
     return 0;
 }
