68cb0ff9ac375f7962450ee2ca01058064ac0fe0
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-20 23:33:44 -0500

    Enunciados del ejercicio completo

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main(){
 
     int P, n, F, frecuencia[101] = {0};
+    cout << "Ingrese el numero de profesores y alumnos: ";
     cin >> P >> n;
 
     for(int i = 0; i < n; i++){
+        cout << "Ingrese el voto: ";
         cin >> F;
         frecuencia[F]++;
     }
-     for(int i = 1; i <= P; i++){
+    cout << "***El resultado es***\n";
+    for(int i = 1; i <= P; i++){
         cout << i << "-" << frecuencia[i] << '\n';
     }
 
