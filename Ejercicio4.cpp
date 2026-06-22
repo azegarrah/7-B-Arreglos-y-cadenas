1998a8a4d04242efddd3c9ce18f1c671e1715788
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-21 19:43:32 -0500

    Corregimos el arreglo porque las variables globales se inician con 0

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int vasos[1000001] = {0};
+int vasos[1000001];
 
 int main(){
 
     int N, C, x;
     cin >> N >> C;
 
-    
-
-
-
+    for(int i = 0; i < C; i++){
+        cin >> x;
+        vasos[x]++;
+    }
 
 
     return 0;
