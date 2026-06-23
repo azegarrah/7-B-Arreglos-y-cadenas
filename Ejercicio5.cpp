bdf3b67551fa39247c73100c200926151284813e
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-22 22:10:01 -0500

    Uso de dos for uno dentro de otro para leer la matriz con sus filas (i) y columnas (j)

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -9,9 +9,16 @@ int main(){
     int n;
     cin >> n;
 
-
-
-
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> A[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            B[j][n - 1 - i] = A[i][j];
+        }
+    }
 
     return 0;
 }
