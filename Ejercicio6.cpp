d86d7eb3eaf6da81b8d2f5a856537c383672936a
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-23 23:32:15 -0500

    Uso del for para que recorra todos los caracteres del arreglo y del if para verificar si el caracter es una letra minuscula

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -8,16 +8,15 @@ int main(){
     char letras[101];
     int n = strlen(texto);
     int k = 0;
-    
+
     cin.getline(texto, 101);
 
-
-
-
-
-
-
-
+    for(int i = 0; i < n; i++){
+        if (texto[i] >= 'a' && texto[i] <= 'z'){
+            letras[k] = texto[i];
+            k = k + 1;
+        }
+    }
 
     return 0;
 }
