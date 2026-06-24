87abc7738f64372256010daf46a38b797e5f35a9
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-23 23:45:37 -0500

    El primer for recorre en cada ciclo ordena las letras, luego en el segundo for compara las letras consecutivas, para luego que en el if compara si la letra de la izquierda es mayor que la derecha; y dentro del if ocurre el intercambio de las letras

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -17,6 +17,15 @@ int main(){
             k = k + 1;
         }
     }
-
+    for(int i = 0; i < k - 1; i++){
+        for(int j = 0; j < k - 1; j++){
+            if(letras[j] > letras[j + 1]){
+                char aux = letras[j];
+                letras[j] = letras[j + 1];
+                letras[j + 1] = aux;
+            }
+        }
+    }
+    
     return 0;
 }
